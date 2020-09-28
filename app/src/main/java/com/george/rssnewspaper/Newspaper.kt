fcaa930f42a896c513efea61662730d703f67753
George Magpie <prich845@gmail.com>
2020-09-28 16:22:36 +0300

    Refactor

--- app/src/main/java/com/george/rssnewspaper/Newspaper.kt
+++ app/src/main/java/com/george/rssnewspaper/Newspaper.kt
@@ -3,9 +3,9 @@ package com.george.rssnewspaper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Newspaper : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_newspaper)
     }
 }
