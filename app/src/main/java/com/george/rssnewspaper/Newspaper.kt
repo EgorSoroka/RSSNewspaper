1d0b8da5eed7aa06b9cf1ae46a849838b057f6c9
George Magpie <prich845@gmail.com>
2020-09-29 21:49:36 +0300

    Возьня

--- app/src/main/java/com/george/rssnewspaper/Newspaper.kt
+++ app/src/main/java/com/george/rssnewspaper/Newspaper.kt
@@ -2,10 +2,16 @@ package com.george.rssnewspaper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Newspaper : AppCompatActivity() {
+    private var buttonAbout:Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_newspaper)
+
     }
+
+
 }
